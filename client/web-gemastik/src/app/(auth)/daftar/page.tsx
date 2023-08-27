49035f9c08ec7b4da9957066d2eb7c18bb600f74
Steven Chan <stevenbelvachann@gmail.com>
2023-08-28 07:28:14 +0800

    signup page style

--- client/web-gemastik/src/app/(auth)/daftar/page.tsx
+++ client/web-gemastik/src/app/(auth)/daftar/page.tsx
@@ -2,15 +2,15 @@ import SignupForm from "@/app/components/Signup/SignupForm"
 
 export default function RegisterPage() {
   return(
-    <div className="flex flex-col lg:flex-row justify-center">
-      <div className="basis-1/2">
-        <p className="text-5xl">
+    <div className="container mx-auto my-24 flex flex-col lg:flex-row justify-center items-center lg:items-start">
+      <div className="basis-1/2 my-8 lg:my-0 lg:pe-4">
+        <p className="text-5xl text-center lg:text-start font-black">
           It’s Our <span className="text-primary-text-green">Responsibility</span> to Keep Earth Clean and Healthy
         </p>
       </div>
 
-      <div className="flex flex-col p-8 h-[625px] w-[500px] bg-[#FAEADE] border-4 border-[#D6925C] rounded-[32px]">
-        <div className="text-primary-text-black mb-8">
+      <div className="flex flex-col p-8 h-[625px] w-[500px] bg-[#FAEADE] border-4 border-[#D6925C] rounded-[32px] shadow-[7px_7px_7px_0_rgba(0,0,0,0.3)]">
+        <div className="mb-8">
           <h2 className="text-4xl font-bold mb-12">Ayo Menjadi <span className="text-primary-text-green">Relawan</span></h2>
           <p className="text-lg font-bold text-primary-text-black/60">Bergabung bersama kami sebagai relawan bumi. Masukkan identitas sesuai formulir di bawah dan jadilah bagian dari kami!</p>
         </div>
